a6beb8accae5acd97a460bdfcdbc225d3324e2e5
timmyRS <github@timmyrs.de>
2018-11-01 12:33:46 +0100

    Add 64-bit check

--- Phpcraft.php
+++ Phpcraft.php
@@ -13,9 +13,13 @@ if(empty($argv))
 {
 	die("This is for CLI PHP. Connect to your server via SSH and use `php ".get_included_files()[0]."` to execute it.\n");
 }
+if(PHP_INT_SIZE < 8)
+{
+	die("Phpcraft requires 64-bit PHP.\n");
+}
 if(version_compare(phpversion(), "7.0.15", "<"))
 {
-	die("Phpcraft requires PHP 7.0.15 or above.\n");
+	die("Phpcraft requires PHP 7.0.15 or above. Try `apt-get install php7.0-cli`.\n");
 }
 if(!extension_loaded("mbstring"))
 {
