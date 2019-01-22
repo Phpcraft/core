d8d78daff95ba32bf0e71b66a1382ce2939812af
timmyRS <github@timmyrs.de>
2019-01-22 09:02:53 +0100

    update docs

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -6,6 +6,5 @@ var searchData=
   ['add',['add',['../classPhpcraft_1_1PlainUserInterface.html#ac0717089ddd1868035934e42e31d0edf',1,'Phpcraft\PlainUserInterface\add()'],['../classPhpcraft_1_1UserInterface.html#ad435b45d17debdda4fcf3ee94a0c38a6',1,'Phpcraft\UserInterface\add()']]],
   ['addhypenstouuid',['addHypensToUUID',['../classPhpcraft_1_1Phpcraft.html#a80e06c8bb9b24299117d34e01932da5c',1,'Phpcraft::Phpcraft']]],
   ['addsupportedprotocolversion',['addSupportedProtocolVersion',['../classPhpcraft_1_1Phpcraft.html#ae3885433d15b897f290a831cbd813327',1,'Phpcraft::Phpcraft']]],
-  ['append',['append',['../classPhpcraft_1_1PlainUserInterface.html#a9b19ec75f626b82f1db7a2a4a20cb93b',1,'Phpcraft\PlainUserInterface\append()'],['../classPhpcraft_1_1UserInterface.html#a326048e06cf1eaea72b01fd4c4a453ed',1,'Phpcraft\UserInterface\append()']]],
-  ['autoload_2ephp',['autoload.php',['../autoload_8php.html',1,'']]]
+  ['append',['append',['../classPhpcraft_1_1PlainUserInterface.html#a9b19ec75f626b82f1db7a2a4a20cb93b',1,'Phpcraft\PlainUserInterface\append()'],['../classPhpcraft_1_1UserInterface.html#a326048e06cf1eaea72b01fd4c4a453ed',1,'Phpcraft\UserInterface\append()']]]
 ];
--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['account_2eclass_2ephp',['Account.class.php',['../Account_8class_8php.html',1,'']]],
-  ['autoload_2ephp',['autoload.php',['../autoload_8php.html',1,'']]]
+  ['account_2eclass_2ephp',['Account.class.php',['../Account_8class_8php.html',1,'']]]
 ];
