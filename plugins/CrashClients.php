ca837bdfad08b6a54c39a4647377186a5eb2e8cf
timmyRS <github@timmyrs.de>
2019-02-27 03:37:06 +0100

    Update CrashClients

--- plugins/CrashClients.php
+++ plugins/CrashClients.php
@@ -17,11 +17,39 @@ PluginManager::registerPlugin("CrashClients", function($plugin)
 		if($event->data["message"] == "crash me")
 		{
 			$con = $event->data["client"];
-			echo $con->username." requested a crash.\n";
+			echo $con->username." requested a crash\n";
+			if($con->protocol_version < 315)
+			{
+				$con->startPacket("set_slot");
+				$con->writeByte(0);
+				$con->writeShort(36);
+				$con->writeShort(0);
+				$con->writeByte(0);
+				$con->writeShort(0);
+				$con->writeByte(0);
+				$con->send();
+				$con->startPacket("spawn_player");
+				$con->writeVarInt(1338);
+				for($i = 0; $i < 16; $i++)
+				{
+					$con->writeByte(0);
+				}
+				$con->writeInt(0);
+				$con->writeInt(50);
+				$con->writeInt(0);
+				$con->writeByte(0);
+				$con->writeByte(0);
+				$con->writeShort(-1);
+				$con->writeByte(127);
+				$con->send();
+			}
 			$con->startPacket("change_game_state");
 			$con->writeByte(7);
 			$con->writeFloat(1337);
 			$con->send();
+			$packet = new \Phpcraft\JoinGamePacket();
+			$packet->dimension = 1337;
+			$packet->send($con);
 			$event->cancel();
 		}
 	});
