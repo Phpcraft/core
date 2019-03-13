e3753a7fa65cb89bcde8f508d48834cee30453b7
timmyRS <github@timmyrs.de>
2019-03-13 16:15:31 +0100

    Move entity metadata test

--- tests/GeneralTest.php
+++ tests/GeneralTest.php
@@ -45,4 +45,22 @@ final class GeneralTest extends \PHPUnit\Framework\TestCase
 		$con->read_buffer = $con->write_buffer;
 		$this->assertEquals($name, $con->readSlot()->getDisplayName());
 	}
+
+	function testEntityMetadata()
+	{
+		$metadata = new \Phpcraft\EntityBase();
+		$metadata->burning = true;
+		$metadata->elytraing = true;
+		$metadata->custom_name = ["text" => "Test", "color" => "yellow"];
+		$con = new \Phpcraft\Connection(47);
+		$metadata->write($con);
+		$this->assertTrue(strpos($con->write_buffer, "§eTest") !== false);
+		$con->read_buffer = $con->write_buffer;
+		$read_metadata = (new \Phpcraft\EntityBase())->read($con);
+		$this->assertTrue($read_metadata->burning);
+		$this->assertFalse($read_metadata->crouching);
+		$this->assertFalse($read_metadata->elytraing);
+		$this->assertEquals($metadata->custom_name, $read_metadata->custom_name);
+		$this->assertNull($read_metadata->silent);
+	}
 }
