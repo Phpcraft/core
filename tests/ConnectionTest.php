cc9d8d9eaaa9cfa4f699755e6bd561c2ef816068
timmyRS <github@timmyrs.de>
2019-03-22 23:14:26 +0100

    Fix failing unit test

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -9,10 +9,9 @@ final class ConnectionTest extends \PHPUnit\Framework\TestCase
 		$con->writeInt(-1);
 		$con->writeInt(3405691582);
 		$con->writeInt(3405691582, true);
-		$this->assertEquals("\x00\x00\x00\x01\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xCA\xFE\xBA\xBE\xCA\xFE\xBA\xBE", $con->write_buffer);
+		$this->assertEquals("\x00\x00\x00\x01\xFF\xFF\xFF\xFF\xCA\xFE\xBA\xBE\xCA\xFE\xBA\xBE", $con->write_buffer);
 		$con->read_buffer = $con->write_buffer;
 		$this->assertEquals(1, $con->readInt());
-		$this->assertEquals(-1, $con->readInt(true));
 		$this->assertEquals(-1, $con->readInt(true));
 		$this->assertEquals(3405691582, $con->readInt());
 		$this->assertEquals(-889275714, $con->readInt(true));
