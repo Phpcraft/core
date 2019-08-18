d2fbbd8c3846088f9cc83dabed261b5b1ab73c17
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-08-19 01:53:45 +0200

    Allow input to be piped into snbt.php

--- snbt.php
+++ snbt.php
@@ -1,12 +1,32 @@
 <?php
 require "vendor/autoload.php";
+use Phpcraft\Phpcraft;
 if(empty($argv[1]))
 {
-	die("Syntax: snbt.php <snbt>\n");
+	$in = "";
+	if(!Phpcraft::isWindows())
+	{
+		$fh = fopen("php://stdin", "r");
+		stream_set_blocking($fh, false);
+		$in = stream_get_contents($fh);
+	}
+	if($in === "")
+	{
+		echo "Syntax: snbt.php <snbt>\n";
+		if(!Phpcraft::isWindows())
+		{
+			echo "or: echo \"...\" | php snbt.php\n";
+		}
+		exit;
+	}
+}
+else
+{
+	$in = join(" ", array_slice($argv, 1));
 }
 use Phpcraft\
 {Connection, Nbt\NbtTag};
-$tag = NbtTag::fromSNBT(join(" ", array_slice($argv, 1)));
+$tag = NbtTag::fromSNBT($in);
 echo "::: Pretty SNBT\n";
 echo $tag->toSNBT(true)."\n";
 echo "::: String Dump\n";
