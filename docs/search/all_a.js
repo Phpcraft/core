39a4b99112aebb3914269add4e47e7da49b548a4
automatic-beyond-belief <github-bot@timmyrs.de>
2019-08-20 18:15:31 +0000

    Update docs

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -14,6 +14,7 @@ var searchData=
   ['isfinished',['isFinished',['../classPhpcraft_1_1Command_1_1ArgumentProvider.html#ac50f1c4d88e225a503e5f177aac7c8a5',1,'Phpcraft::Command::ArgumentProvider']]],
   ['isonline',['isOnline',['../classPhpcraft_1_1Account.html#a6f7df18e660e7643779a467352f3da56',1,'Phpcraft::Account']]],
   ['isonlinemode',['isOnlineMode',['../classPhpcraft_1_1Server.html#a80c0df387dc345950a7599b800bf27c9',1,'Phpcraft::Server']]],
+  ['isop',['isOP',['../classPhpcraft_1_1ClientConnection.html#a05667696a9b1559d7abd14212738db8a',1,'Phpcraft\ClientConnection\isOP()'],['../interfacePhpcraft_1_1Command_1_1CommandSender.html#af3ceaa3606727f61f3d6954d93d0730d',1,'Phpcraft\Command\CommandSender\isOP()'],['../classPhpcraft_1_1Server.html#a5be58c7d62ea737a039c58fcb212a09a',1,'Phpcraft\Server\isOP()']]],
   ['isopen',['isOpen',['../classPhpcraft_1_1Connection.html#a97d64586a07790901e0e9dc83a9cfd66',1,'Phpcraft\Connection\isOpen()'],['../classPhpcraft_1_1Server.html#a75d9cf1effa434c6ec3f967eb97c6254',1,'Phpcraft\Server\isOpen()']]],
   ['isvalidbarestring',['isValidBareString',['../classPhpcraft_1_1Nbt_1_1NbtTag.html#af3d1fcbb33919d8dad434530d7d01875',1,'Phpcraft::Nbt::NbtTag']]],
   ['iswindows',['isWindows',['../classPhpcraft_1_1Phpcraft.html#a5e7739b3a11a57b71a05ecb4b0648d31',1,'Phpcraft::Phpcraft']]],
--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['binarystringtohex',['binaryStringToHex',['../classPhpcraft_1_1Phpcraft.html#a31fe0ef2465673ad8f42f1c418ed1fe9',1,'Phpcraft::Phpcraft']]],
-  ['broadcast',['broadcast',['../classPhpcraft_1_1Server.html#a021d35af4150e5325fb90ab13261c1ac',1,'Phpcraft::Server']]],
+  ['broadcast',['broadcast',['../classPhpcraft_1_1Server.html#a599d09f219a491010b31072390556179',1,'Phpcraft::Server']]],
   ['buildlegacyassetsfolder',['buildLegacyAssetsFolder',['../classPhpcraft_1_1AssetsManager.html#aed92d67b2b8e22dac0c0a17456f09cea',1,'Phpcraft::AssetsManager']]]
 ];
