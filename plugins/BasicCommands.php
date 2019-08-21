a1093ec1ed8ec49c9fbf010d36ae87a44cbc6038
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-08-21 20:16:52 +0200

    [BasicCommands] Create custom GamemodeArgument for /gamemode command

--- plugins/BasicCommands.php
+++ plugins/BasicCommands.php
@@ -5,7 +5,45 @@
  * @var Plugin $this
  */
 use Phpcraft\
-{ClientConnection, Command\CommandSender, Plugin\Plugin, Plugin\PluginManager};
+{ClientConnection, Command\ArgumentProvider, Command\CommandSender, Enum\Gamemode, Plugin\Plugin, Plugin\PluginManager};
+if(!class_exists("GamemodeArgument"))
+{
+	class GamemodeArgument
+	{
+		public $gamemode;
+
+		function __construct(int $gamemode)
+		{
+			$this->gamemode = $gamemode;
+		}
+	}
+
+	class GamemodeArgumentProvider extends ArgumentProvider
+	{
+		private $value;
+
+		public function __construct(string $arg)
+		{
+			if(is_numeric($arg) && $arg >= 0 && $arg <= 4)
+			{
+				$this->value = $arg;
+			}
+			else if(Gamemode::validateName(strtoupper($arg)))
+			{
+				$this->value = Gamemode::valueOf(strtoupper($arg));
+			}
+			else
+			{
+				throw new DomainException("Invalid gamemode: ".$arg);
+			}
+		}
+
+		function getValue(): GamemodeArgument
+		{
+			return new GamemodeArgument($this->value);
+		}
+	}
+}
 $this->registerCommand("help", function(CommandSender &$sender)
 {
 	$commands = [];
@@ -18,19 +56,19 @@ $this->registerCommand("help", function(CommandSender &$sender)
 	 ->registerCommand([
 		 "gamemode",
 		 "gm"
-	 ], function(CommandSender &$client, int $gamemode)
+	 ], function(CommandSender &$client, GamemodeArgument $gamemode)
 	 {
 		 if(!$client instanceof ClientConnection)
 		 {
 			 $client->sendMessage("This command is only for players.");
 			 return;
 		 }
-		 $client->setGamemode($gamemode);
+		 $client->setGamemode($gamemode->gamemode);
 		 $client->startPacket("player_info");
 		 $client->writeVarInt(1);
 		 $client->writeVarInt(1);
 		 $client->writeUUID($client->uuid);
-		 $client->writeVarInt($gamemode);
+		 $client->writeVarInt($gamemode->gamemode);
 		 $client->send();
 	 })
 	 ->registerCommand("abilities", function(CommandSender &$client, $abilities)
