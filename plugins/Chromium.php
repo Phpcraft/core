76f455823e13205cf8013d4a014b280424a96f65
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-08-29 20:28:59 +0200

    Update Chromium plugin

--- plugins/Chromium.php
+++ plugins/Chromium.php
@@ -19,7 +19,7 @@ if(!extension_loaded("gd"))
 use pac\
 {Chromium, Page};
 use Phpcraft\
-{ClientConnection, Command\CommandSender, Event\ServerTickEvent, Item, Nbt\NbtCompound, Nbt\NbtInt, Nbt\NbtString, Packet\MapDataPacket, Packet\SetSlotPacket, Plugin\Plugin, Slot};
+{ClientConnection, Command\CommandSender, Event\ServerJoinEvent, Event\ServerLeaveEvent, Event\ServerTickEvent, Item, Nbt\NbtCompound, Nbt\NbtInt, Nbt\NbtString, Packet\MapDataPacket, Packet\SetSlotPacket, Plugin\Plugin, Slot};
 $c = new Chromium();
 if(!$c->isAvailable())
 {
@@ -27,68 +27,128 @@ if(!$c->isAvailable())
 	$c->download();
 	echo " Done.\n";
 }
-$i = $c->start();
+$client_pages = [];
+$i = $c->start(false);
 //$i->logging = true;
-$this->registerCommand(["open", "navigate"], function(CommandSender $con, string $url, float $scale = 1) use (&$i)
+$this->registerCommand("scale", function(CommandSender $con, float $scale)
 {
 	if(!$con instanceof ClientConnection)
 	{
 		$con->sendMessage("This command is only for players.");
 		return;
 	}
-	$i->newPage(function(Page $page) use (&$con, &$url, &$scale)
+	global $client_pages;
+	$client_pages[$con->username][1] = false;
+	$client_pages[$con->username][0]->setDeviceMetrics(128 * (1 / $scale), 128 * (1 / $scale), $scale, function() use (&$con)
 	{
-		$page->once("Page.frameStoppedLoading", function() use (&$con, &$url, &$scale, &$page)
-		{
-			$page->setDeviceMetrics(128 * (1 / $scale), 128 * (1 / $scale), $scale, function() use (&$con, &$url, &$page)
-			{
-				$page->captureScreenshot("png", [], function($data) use (&$con, &$url, &$page)
-				{
-					$page->close();
-					$id = rand(0, 35536);
-					(new SetSlotPacket(0, Slot::HOTBAR_2, Item::get("filled_map")
-															  ->slot(1, new NbtCompound("tag", [
-																  new NbtCompound("display", [
-																	  new NbtString("Name", json_encode(["text" => $url]))
-																  ]),
-																  new NbtInt("map", $id)
-															  ]))))->send($con);
-					$packet = new MapDataPacket();
-					$packet->mapId = $id;
-					$packet->width = 128;
-					$packet->height = 128;
-					$img = imagecreatefromstring(base64_decode($data));
-					for($y = 0; $y < 128; $y++)
-					{
-						for($x = 0; $x < 128; $x++)
-						{
-							$rgb = imagecolorat($img, $x, $y);
-							$r = ($rgb >> 16) & 0xFF;
-							$g = ($rgb >> 8) & 0xFF;
-							$b = $rgb & 0xFF;
-							array_push($packet->contents, MapDataPacket::getColorId([
-								$r,
-								$g,
-								$b
-							]));
-						}
-					}
-					$packet->send($con);
-				});
-			});
-		})
-			 ->navigate($url);
+		global $client_pages;
+		$client_pages[$con->username][1] = true;
 	});
+});
+$this->registerCommand("goto", function(CommandSender $con, string $url)
+{
+	if(!$con instanceof ClientConnection)
+	{
+		$con->sendMessage("This command is only for players.");
+		return;
+	}
+	global $client_pages;
+	$client_pages[$con->username][1] = false;
+	$client_pages[$con->username][0]->once("Page.frameStoppedLoading", function() use (&$con)
+	{
+		global $client_pages;
+		$client_pages[$con->username][1] = true;
+	})
+									->navigate($url);
+});
+$this->on(function(ServerJoinEvent $event) use (&$i)
+{
+	$i->newPage(function(Page $page) use (&$event)
+	{
+		$page->setDeviceMetrics(512, 512, 0.25, function() use (&$event, &$page)
+		{
+			$page->navigate("https://github.com/timmyRS/Phpcraft", function() use (&$event, &$page)
+			{
+				global $client_pages;
+				$client_pages[$event->client->username] = [
+					$page,
+					true
+				];
+				(new SetSlotPacket(0, Slot::HOTBAR_3, Item::get("filled_map")
+														  ->slot(1, new NbtCompound("tag", [
+															  new NbtCompound("display", [
+																  new NbtString("Name", json_encode(["text" => "Chromium"]))
+															  ]),
+															  new NbtInt("map", 1338)
+														  ]))))->send($event->client);
+			});
+		});
+	});
+});
+$this->on(function(ServerLeaveEvent $event)
+{
+	global $client_pages;
+	if(@array_key_exists($event->client->username, $client_pages))
+	{
+		$client_pages[$event->client->username]->close();
+		unset($client_pages[$event->client->username]);
+	}
 });
 $this->on(function(ServerTickEvent $event) use (&$i)
 {
-	if($i->isRunning())
+	if(!$i->isRunning())
 	{
-		$i->handle();
-	}
-	else
-	{
-		echo "Chromium process terminated, unregistering plugin.\n";
+		echo "[Chromium] Chromium process is no longer running.\n";
 		$this->unregister();
 	}
+	$i->handle();
+	global $client_pages;
+	foreach($event->server->getPlayers() as $client)
+	{
+		if(!@array_key_exists($client->username, $client_pages))
+		{
+			continue;
+		}
+		if(!$client_pages[$client->username][1])
+		{
+			continue;
+		}
+		$client_pages[$client->username][1] = false;
+		$client_pages[$client->username][0]->captureScreenshot("png", [], function($data) use (&$client)
+		{
+			$packet = new MapDataPacket();
+			$packet->mapId = 1338;
+			$packet->width = 128;
+			$packet->height = 128;
+			$img = imagecreatefromstring(base64_decode($data));
+			for($y = 0; $y < 128; $y++)
+			{
+				for($x = 0; $x < 128; $x++)
+				{
+					$rgb = imagecolorat($img, $x, $y);
+					$r = ($rgb >> 16) & 0xFF;
+					$g = ($rgb >> 8) & 0xFF;
+					$b = $rgb & 0xFF;
+					array_push($packet->contents, MapDataPacket::getColorId([
+						$r,
+						$g,
+						$b
+					]));
+				}
+			}
+			$packet->send($client);
+			global $client_pages;
+			$client_pages[$client->username][1] = true;
+		});
+	}
+	$i->handle();
+});
+$this->registerCommand("close_chromium", function(CommandSender $sender) use (&$i)
+{
+	if(!$sender->isOP())
+	{
+		$sender->sendMessage("This command is only for OPs.");
+		return;
+	}
+	$i->close();
 });
