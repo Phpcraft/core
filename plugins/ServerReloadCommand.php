e979c06cd9a887b58ccbc5b51c7352b5d802b640
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-09-09 15:24:10 +0200

    Fix /reload

--- plugins/ServerReloadCommand.php
+++ plugins/ServerReloadCommand.php
@@ -8,16 +8,8 @@ use Phpcraft\
 {Command\CommandSender, Plugin\Plugin, Plugin\PluginManager};
 $this->registerCommand("reload", function(CommandSender &$sender)
 {
-	if(!$sender->isOP())
-	{
-		$sender->sendMessage([
-			"text" => "You need to be OP in order to use this command.",
-			"color" => "red"
-		]);
-		return;
-	}
 	PluginManager::unloadAllPlugins();
 	echo "Unloaded all plugins.\nLoading plugins...\n";
 	PluginManager::loadPlugins();
 	echo count(PluginManager::$loaded_plugins)." plugins loaded.\n";
-});
+}, "use /reload");
