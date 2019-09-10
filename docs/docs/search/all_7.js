a044d6441de70c9dfa5aaeb848193197b8d2410b
automatic-beyond-belief <github-bot@timmyrs.de>
2019-09-10 02:05:23 +0000

    Update docs

--- docs/docs/search/all_7.js
+++ docs/docs/search/all_7.js
@@ -8,6 +8,7 @@ var searchData=
   ['floatargumentprovider_2ephp',['FloatArgumentProvider.php',['../FloatArgumentProvider_8php.html',1,'']]],
   ['format_5fampersand',['FORMAT_AMPERSAND',['../classPhpcraft_1_1Phpcraft.html#a3f95aeb3ebd5b39428049147a1187a23',1,'Phpcraft::Phpcraft']]],
   ['format_5fansi',['FORMAT_ANSI',['../classPhpcraft_1_1Phpcraft.html#a711c879e0827b293c55181dabd86f950',1,'Phpcraft::Phpcraft']]],
+  ['format_5fhtml',['FORMAT_HTML',['../classPhpcraft_1_1Phpcraft.html#a2fb3f363344ee5000add61bd17693771',1,'Phpcraft::Phpcraft']]],
   ['format_5fnone',['FORMAT_NONE',['../classPhpcraft_1_1Phpcraft.html#a711cebd92edfd78135b128d4585e178b',1,'Phpcraft::Phpcraft']]],
   ['format_5fsilcrow',['FORMAT_SILCROW',['../classPhpcraft_1_1Phpcraft.html#aadecd1d7bb833a1504b554eea41df74b',1,'Phpcraft::Phpcraft']]],
   ['fromminecraftversion',['fromMinecraftVersion',['../classPhpcraft_1_1AssetsManager.html#a37b69586a1ec2cd6015d43f549ca4b40',1,'Phpcraft::AssetsManager']]],
--- docs/docs/search/variables_5.js
+++ docs/docs/search/variables_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['feet',['FEET',['../classPhpcraft_1_1Slot.html#a43d1ef95a8e832bdd8fa686905ec2c4f',1,'Phpcraft::Slot']]],
   ['format_5fampersand',['FORMAT_AMPERSAND',['../classPhpcraft_1_1Phpcraft.html#a3f95aeb3ebd5b39428049147a1187a23',1,'Phpcraft::Phpcraft']]],
   ['format_5fansi',['FORMAT_ANSI',['../classPhpcraft_1_1Phpcraft.html#a711c879e0827b293c55181dabd86f950',1,'Phpcraft::Phpcraft']]],
+  ['format_5fhtml',['FORMAT_HTML',['../classPhpcraft_1_1Phpcraft.html#a2fb3f363344ee5000add61bd17693771',1,'Phpcraft::Phpcraft']]],
   ['format_5fnone',['FORMAT_NONE',['../classPhpcraft_1_1Phpcraft.html#a711cebd92edfd78135b128d4585e178b',1,'Phpcraft::Phpcraft']]],
   ['format_5fsilcrow',['FORMAT_SILCROW',['../classPhpcraft_1_1Phpcraft.html#aadecd1d7bb833a1504b554eea41df74b',1,'Phpcraft::Phpcraft']]]
 ];
