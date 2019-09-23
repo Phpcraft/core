7b955199024ee95d2c01bbb227d61a996d2c1b7c
automatic-beyond-belief <github-bot@timmyrs.de>
2019-09-23 09:47:18 +0000

    Update docs

--- docs/docs/search/classes_10.js
+++ docs/docs/search/classes_10.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['server',['Server',['../classPhpcraft_1_1Server.html',1,'Phpcraft\Server'],['../classPhpcraft_1_1Realms_1_1Server.html',1,'Phpcraft\Realms\Server']]],
+  ['server',['Server',['../classPhpcraft_1_1Realms_1_1Server.html',1,'Phpcraft\Realms\Server'],['../classPhpcraft_1_1Server.html',1,'Phpcraft\Server']]],
   ['serverboundbrandpluginmessagepacket',['ServerboundBrandPluginMessagePacket',['../classPhpcraft_1_1Packet_1_1PluginMessage_1_1ServerboundBrandPluginMessagePacket.html',1,'Phpcraft::Packet::PluginMessage']]],
   ['serverboundpacket',['ServerboundPacket',['../classPhpcraft_1_1Packet_1_1ServerboundPacket.html',1,'Phpcraft::Packet']]],
   ['serverboundpluginmessagepacket',['ServerboundPluginMessagePacket',['../classPhpcraft_1_1Packet_1_1PluginMessage_1_1ServerboundPluginMessagePacket.html',1,'Phpcraft::Packet::PluginMessage']]],
   ['serverboundstringpluginmessagepacket',['ServerboundStringPluginMessagePacket',['../classPhpcraft_1_1Packet_1_1PluginMessage_1_1ServerboundStringPluginMessagePacket.html',1,'Phpcraft::Packet::PluginMessage']]],
   ['serverchatevent',['ServerChatEvent',['../classPhpcraft_1_1Event_1_1ServerChatEvent.html',1,'Phpcraft::Event']]],
+  ['serverchunkborderevent',['ServerChunkBorderEvent',['../classPhpcraft_1_1Event_1_1ServerChunkBorderEvent.html',1,'Phpcraft::Event']]],
   ['serverclientevent',['ServerClientEvent',['../classPhpcraft_1_1Event_1_1ServerClientEvent.html',1,'Phpcraft::Event']]],
   ['serverconnection',['ServerConnection',['../classPhpcraft_1_1ServerConnection.html',1,'Phpcraft']]],
   ['serverconsoleevent',['ServerConsoleEvent',['../classPhpcraft_1_1Event_1_1ServerConsoleEvent.html',1,'Phpcraft::Event']]],
@@ -13,6 +14,7 @@ var searchData=
   ['serverflyingchangeevent',['ServerFlyingChangeEvent',['../classPhpcraft_1_1Event_1_1ServerFlyingChangeEvent.html',1,'Phpcraft::Event']]],
   ['serverjoinevent',['ServerJoinEvent',['../classPhpcraft_1_1Event_1_1ServerJoinEvent.html',1,'Phpcraft::Event']]],
   ['serverleaveevent',['ServerLeaveEvent',['../classPhpcraft_1_1Event_1_1ServerLeaveEvent.html',1,'Phpcraft::Event']]],
+  ['servermovementevent',['ServerMovementEvent',['../classPhpcraft_1_1Event_1_1ServerMovementEvent.html',1,'Phpcraft::Event']]],
   ['serverongroundchangeevent',['ServerOnGroundChangeEvent',['../classPhpcraft_1_1Event_1_1ServerOnGroundChangeEvent.html',1,'Phpcraft::Event']]],
   ['serverpacketevent',['ServerPacketEvent',['../classPhpcraft_1_1Event_1_1ServerPacketEvent.html',1,'Phpcraft::Event']]],
   ['servertickevent',['ServerTickEvent',['../classPhpcraft_1_1Event_1_1ServerTickEvent.html',1,'Phpcraft::Event']]],
