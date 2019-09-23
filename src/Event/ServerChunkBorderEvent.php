dd71d65d6bab67066b022649fe4a5d09a7d4797a
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-09-23 12:35:08 +0200

    Allow prev_ values to be null

--- src/Event/ServerChunkBorderEvent.php
+++ src/Event/ServerChunkBorderEvent.php
@@ -9,17 +9,24 @@ class ServerChunkBorderEvent extends ServerMovementEvent
 	/**
 	 * The client's chunk x position before this event.
 	 *
-	 * @var int $prev_chunk_x
+	 * @var int|null $prev_chunk_x
 	 */
 	public $prev_chunk_x;
 	/**
 	 * The client's chunk z position before this event.
 	 *
-	 * @var int $prev_chunk_z
+	 * @var int|null $prev_chunk_z
 	 */
 	public $prev_chunk_z;
 
-	function __construct(Server $server, ClientConnection $client, Position $prev_pos, int $prev_chunk_x, int $prev_chunk_z)
+	/**
+	 * @param Server $server
+	 * @param ClientConnection $client
+	 * @param Position|null $prev_pos
+	 * @param int|null $prev_chunk_x
+	 * @param int|null $prev_chunk_z
+	 */
+	function __construct(Server $server, ClientConnection $client, $prev_pos, $prev_chunk_x, $prev_chunk_z)
 	{
 		parent::__construct($server, $client, $prev_pos);
 		$this->prev_chunk_x = $prev_chunk_x;
--- src/Event/ServerMovementEvent.php
+++ src/Event/ServerMovementEvent.php
@@ -8,11 +8,16 @@ class ServerMovementEvent extends ServerClientEvent
 	/**
 	 * The client's position before this event.
 	 *
-	 * @var Position $prev_pos
+	 * @var Position|null $prev_pos
 	 */
 	public $prev_pos;
 
-	function __construct(Server $server, ClientConnection $client, Position $prev_pos)
+	/**
+	 * @param Server $server
+	 * @param ClientConnection $client
+	 * @param Position|null $prev_pos
+	 */
+	function __construct(Server $server, ClientConnection $client, $prev_pos)
 	{
 		parent::__construct($server, $client);
 		$this->prev_pos = $prev_pos;
