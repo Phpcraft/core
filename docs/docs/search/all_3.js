b3a60fbd3c450545532f4a2e9a4da0c3ed292887
automatic-beyond-belief <github-bot@timmyrs.de>
2019-10-09 01:09:49 +0000

    Update docs

--- docs/docs/search/all_3.js
+++ docs/docs/search/all_3.js
@@ -3,6 +3,7 @@ var searchData=
   ['bin_5fdir',['BIN_DIR',['../classPhpcraft_1_1Phpcraft.html#a540adc4c91e28f902f8502810b7ba04e',1,'Phpcraft::Phpcraft']]],
   ['binarystringtohex',['binaryStringToHex',['../classPhpcraft_1_1Phpcraft.html#a31fe0ef2465673ad8f42f1c418ed1fe9',1,'Phpcraft::Phpcraft']]],
   ['block',['Block',['../classPhpcraft_1_1Block.html',1,'Phpcraft\Block'],['../classPhpcraft_1_1Point3D.html#a15495f1ca5263cd76e08ec9cf57f5aff',1,'Phpcraft\Point3D\block()']]],
+  ['blockcenter',['blockCenter',['../classPhpcraft_1_1Point3D.html#a18154372ed189abb578c33b4379d6b53',1,'Phpcraft::Point3D']]],
   ['blockproperty',['BlockProperty',['../classPhpcraft_1_1BlockProperty.html',1,'Phpcraft']]],
   ['blockstate',['BlockState',['../classPhpcraft_1_1BlockState.html',1,'Phpcraft']]],
   ['bossbarpacket',['BossBarPacket',['../classPhpcraft_1_1Packet_1_1BossBar_1_1BossBarPacket.html',1,'Phpcraft::Packet::BossBar']]],
