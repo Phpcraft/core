ca1bac33107a307ab134f326532b78d2e98534de
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-11-10 11:56:38 +0100

    Remove Guardian::$is_retracting_spikes

--- src/Entity/Guardian.php
+++ src/Entity/Guardian.php
@@ -6,10 +6,6 @@ use Phpcraft\Exception\IOException;
 class Guardian extends Monster
 {
 	/**
-	 * @var bool|null $is_retracting_spikes
-	 */
-	public $is_retracting_spikes = null;
-	/**
 	 * The ID of the entity being targeted by the guardian. 0 = no entity.
 	 *
 	 * @var GMP|string|int|null $target_eid
@@ -24,23 +20,6 @@ class Guardian extends Monster
 	function write(Connection $con)
 	{
 		parent::write($con);
-		if($this->is_retracting_spikes !== null)
-		{
-			// This probably doesn't work on pvs 57 to 300; Elder Guardian became a seperate entity with pv 301.
-			if($con->protocol_version >= 57)
-			{
-				self::writeBoolean($con, ($con->protocol_version >= 472 ? 14 : 12), true);
-			}
-			else
-			{
-				$flags = $this->is_retracting_spikes ? 0x04 : 0;
-				if($this instanceof ElderGuardian)
-				{
-					$flags |= 0x02;
-				}
-				self::writeByte($con, 16, $flags);
-			}
-		}
 		if($this->target_eid !== null)
 		{
 			self::writeInt($con, ($con->protocol_version >= 57 ? ($con->protocol_version >= 472 ? 15 : 13) : 17), $this->target_eid);
@@ -54,10 +33,6 @@ class Guardian extends Monster
 	function getStringAttributes()
 	{
 		$attr = parent::getStringAttributes();
-		if($this->is_retracting_spikes !== null)
-		{
-			array_push($attr, "Is".($this->is_retracting_spikes ? "" : "not ")." retracting spikes");
-		}
 		if($this->target_eid !== null)
 		{
 			array_push($attr, "Targeting entity ".$this->target_eid);
@@ -77,9 +52,6 @@ class Guardian extends Monster
 		{
 			switch($index)
 			{
-				case ($con->protocol_version >= 472 ? 14 : 12):
-					$this->is_retracting_spikes = $con->readBoolean();
-					return true;
 				case ($con->protocol_version >= 472 ? 15 : 13):
 					$this->target_eid = $con->readVarInt();
 					return true;
@@ -89,9 +61,6 @@ class Guardian extends Monster
 		{
 			switch($index)
 			{
-				case 16:
-					$this->is_retracting_spikes = $con->readByte() & 0x04;
-					return true;
 				case 17:
 					$this->target_eid = $con->readVarInt();
 					return true;
