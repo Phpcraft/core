155f39a6a7c7a6761ca7d4cc08fb697d306496bd
Tim "timmyRS" Speckhals <github@timmyrs.de>
2020-02-29 05:10:45 +0100

    Update BareServer comments

--- src/BareServer.php
+++ src/BareServer.php
@@ -2,14 +2,14 @@
 namespace Phpcraft;
 use Phpcraft\World\World;
 /**
- * The bare minimium of a server, featuring a world and entity tracking.
+ * The bare minimium of a server, featuring worlds and entity tracking.
  *
  * @since 0.5.5
  */
 class BareServer
 {
 	/**
-	 * The world that clients will be presented with.
+	 * An array of worlds that clients can be presented with.
 	 *
 	 * @var World[] $worlds
 	 * @since 0.5.6
